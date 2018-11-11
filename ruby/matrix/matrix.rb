1e44858da4113b676caa2e0cc919be02103cf55f
Andrea Momesso <momesso.andrea@gmail.com>
2018-11-11 17:47:04 +0100

    Improved matrix thanks to attr_accessor

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,13 +1,16 @@
 class Matrix
+  attr_accessor :rows
+  attr_accessor :columns
+
   def initialize(matrix_as_string)
     @matrix_as_string = matrix_as_string
+    @rows = extract_rows(@matrix_as_string)
+    @columns = @rows.transpose
   end
 
-  def rows
-    @matrix_as_string.lines.map { |x| x.split.map(&:to_i) }
+  private
+  def extract_rows(matrix_as_string)
+    matrix_as_string.lines.map { |x| x.split.map(&:to_i) }
   end
 
-  def columns
-    self.rows.transpose
-  end
 end
