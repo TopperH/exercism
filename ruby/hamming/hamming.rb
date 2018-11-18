98d42fbd355f6e22d84e50e6670a83b5a402067d
Andrea Momesso <momesso.andrea@gmail.com>
2018-11-18 18:32:17 +0100

    Added raindrop exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,7 +2,7 @@ module Hamming
   def self.compute(reference_string, submitted_string)
     raise ArgumentError if reference_string.size != submitted_string.size
 
-    reference_string.scan(/\w/).zip(submitted_string.scan(/\w/))
+    reference_string.chars.zip(submitted_string.chars)
                     .count { |a, b| a != b }
   end
 end
