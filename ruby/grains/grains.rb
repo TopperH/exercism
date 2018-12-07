f4180ef09b86d67e0f24cd7f9cbfd308066490c1
Andrea Momesso <momesso.andrea@gmail.com>
2018-12-07 07:07:00 +0100

    Removed useless map, also used Range#cover? instead of #include?

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,12 +1,12 @@
 module Grains
   BOARD_SQUARES = (1..64).freeze
   def self.square(integer)
-    raise ArgumentError unless BOARD_SQUARES.include?(integer)
+    raise ArgumentError unless BOARD_SQUARES.cover?(integer)
 
     integer**(integer - 1)
   end
 
   def self.total
-    BOARD_SQUARES.map { |i| square(i) }.sum
+    BOARD_SQUARES { |i| square(i) }.sum
   end
 end
