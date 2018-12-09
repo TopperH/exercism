66ef1d9d031f6986567300cf8596bfa8c38f73c8
Andrea Momesso <momesso.andrea@gmail.com>
2018-12-09 20:43:19 +0100

    Nice solution for grains -- APPROVED

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,12 +1,12 @@
 module Grains
   BOARD_SQUARES = (1..64).freeze
   def self.square(integer)
-    raise ArgumentError unless BOARD_SQUARES.cover?(integer)
+    raise ArgumentError unless BOARD_SQUARES.include?(integer)
 
-    integer**(integer - 1)
+    2**(integer - 1)
   end
 
   def self.total
-    BOARD_SQUARES { |i| square(i) }.sum
+    (square(BOARD_SQUARES.last) * 2) - 1
   end
 end
