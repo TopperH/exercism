6e21b7140b6b4dea8a363347294f910d1b5ee80b
Andrea Momesso <momesso.andrea@gmail.com>
2019-12-30 12:00:32 +0100

    Final resistor color solution

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,7 @@
 class ResistorColorDuo
-  COLOR_LIST = %w(black brown red orange yellow green blue violet grey white)
+  COLOR_LIST = %w[black brown red orange yellow green blue violet grey white]
   def self.value(color_array)
-    (0..1).map { |index| COLOR_LIST.find_index(color_array[index]).to_s }.join.to_i
+    (0..1).map { |index| COLOR_LIST.find_index(color_array[index]) }.join.to_i
   end
 end
 
