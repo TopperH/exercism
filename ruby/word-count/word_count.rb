df1406aa27e93abf479d6d09580f3788d60821d9
Andrea Momesso <momesso.andrea@gmail.com>
2020-01-01 18:48:27 +0100

    Iteration checking for private contstant

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -5,6 +5,8 @@ class Phrase
     word_even_with_apostrophe: /\b[\w']+\b/,
   }
   
+  private_constant :RegularExpression
+
   attr_reader :sentence
   def initialize(sentence)
     @sentence = sentence
@@ -14,5 +16,17 @@ class Phrase
 
   def word_count
     sentence.downcase.scan(RegularExpression[:word_even_with_apostrophe]).tally
+  end
+end
+
+if defined?(Minitest)
+  describe Phrase do
+    describe :RegularExpression do
+      it 'must be a private constant' do
+        assert_raises NameError do
+          _(Phrase::RegularExpression)
+        end
+      end
+    end
   end
 end
