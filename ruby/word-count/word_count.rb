575ed8819d9976b69f2a93f1ad7dacc36a523b65
Andrea Momesso <momesso.andrea@gmail.com>
2020-01-02 21:03:20 +0100

    New iteration with alias

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,30 +1,30 @@
 class Phrase
-  private
+  RE = RegularExpression = {
+    word_even_with_apostrophe: /\b[\w']+\b/
+  }.freeze
 
-  RegularExpression = {
-    word_even_with_apostrophe: /\b[\w']+\b/,
-  }
-  
   private_constant :RegularExpression
+
+  def word_count
+    sentence.downcase.scan(RE[:word_even_with_apostrophe]).tally
+  end
+
+  private
 
   attr_reader :sentence
   def initialize(sentence)
     @sentence = sentence
   end
-
-  public
-
-  def word_count
-    sentence.downcase.scan(RegularExpression[:word_even_with_apostrophe]).tally
-  end
 end
 
 if defined?(Minitest)
   describe Phrase do
-    describe :RegularExpression do
-      it 'must be a private constant' do
-        assert_raises NameError do
-          _(Phrase::RegularExpression)
+    %w[RE Regularexpression].each do |private_const|
+      describe private_const do
+        it 'must be a private constant' do
+          assert_raises NameError do
+            Phrase.private_const
+          end
         end
       end
     end
